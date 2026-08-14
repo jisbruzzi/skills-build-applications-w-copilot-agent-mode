a8ff7834f2e12cbb22ac1efabfac80dcc7baedbc
José Ignacio Sbruzzi <jose.sbru@gmail.com>
2026-08-14 18:07:54 +0000

    step 4

--- octofit-tracker/backend/dist/config/database.js
+++ octofit-tracker/backend/dist/config/database.js
@@ -4,13 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const app_1 = __importDefault(require("./app"));
-const connectionString = app_1.default.MONGODB_URI;
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/octofit_db';
 const db = mongoose_1.default.connection;
 mongoose_1.default
     .connect(connectionString)
     .then(() => {
-    console.log(`Connected to MongoDB at ${connectionString}`);
+    console.log('Connected to octofit_db');
 })
     .catch((error) => {
     console.error('MongoDB connection error:', error.message || error);
